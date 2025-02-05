126deb84e2b4b11f83d6510962f472b0f8222c38
felipe <jonathanfelipe0111@hotmail.com>
2025-02-05 17:24:23 -0500

    NextJS Fonts Add Assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Font imports
+import { geistMono, geistSans } from "@/config/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
