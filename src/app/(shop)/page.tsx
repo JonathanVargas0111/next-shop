73d421529317b0302f432dc5d466db80e4481f32
felipe <jonathanfelipe0111@hotmail.com>
2025-02-05 17:50:12 -0500

    directory structure first pages

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -3,8 +3,8 @@ import { titleFont } from "@/config/fonts";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       <h2 className={titleFont.className} >Hola con fuente distintas</h2>
-    </main>
+    </div>
   );
 }
